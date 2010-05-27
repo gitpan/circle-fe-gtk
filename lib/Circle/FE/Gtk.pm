b9817499bbf87ef899f4dd536540b8648b7dcf90
Paul Evans <leonerd@leonerd.org.uk>
2010-05-27 12:00:08 +0000

    Import of PEVANS/circle-fe-gtk-0.01 from CPAN.
    gitpan-cpan-distribution: circle-fe-gtk
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PEVANS/circle-fe-gtk-0.01.tar.gz
    gitpan-cpan-author:       PEVANS
    gitpan-cpan-maturity:     released

---
+++ lib/Circle/FE/Gtk.pm
@@ -0,0 +1,19 @@
+#  You may distribute under the terms of the GNU General Public License
+#
+#  (C) Paul Evans, 2008-2010 -- leonerd@leonerd.org.uk
+
+package Circle::FE::Gtk;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+C<Circle::FE::Gtk> - L<Gtk2> frontend for the C<Circle> application host
+
+=head1 AUTHOR
+
+Paul Evans <leonerd@leonerd.org.uk>
